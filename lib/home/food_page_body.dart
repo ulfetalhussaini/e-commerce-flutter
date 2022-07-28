f6d82e8f515db7609f44133ffdd7a48e2cc33663
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2022-07-28 15:27:56 +0300

    first commit

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,160 @@
+import 'package:e_commerce/utils/colors.dart';
+import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/icon_and_text_widget.dart';
+import 'package:e_commerce/widgets/small_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FoodpageBody extends StatefulWidget {
+  const FoodpageBody({Key? key}) : super(key: key);
+
+  @override
+  State<FoodpageBody> createState() => _FoodpageBodyState();
+}
+
+class _FoodpageBodyState extends State<FoodpageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      // color: Colors.redAccent,
+      child: PageView.builder(
+        controller: pageController,
+        itemCount: 5,
+        itemBuilder: (context, position) {
+          return _buildPageItem(position);
+        },
+      ),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/4552564.jpg"))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Chinese Side"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                            5,
+                            (index) => Icon(
+                                  Icons.star,
+                                  color: AppColors.mainColor,
+                                  size: 15,
+                                )),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "4.5"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "1287"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor1),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7km",
+                          iconColor: AppColors.mainColor),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "32min",
+                          iconColor: AppColors.iconColor2),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,62 @@
+import 'package:e_commerce/home/food_page_body.dart';
+import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: "Iraq",
+                        color: AppColors.mainColor,
+                      ),
+                      Row(
+                        children: [
+                          SmallText(text: "Baghdad", color: Colors.black45,),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          FoodpageBody(),
+        ],
+      ),
+    );
+  }
+}
